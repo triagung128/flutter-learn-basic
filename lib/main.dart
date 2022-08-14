9b69659e7581d45dded25fed66aa5a9ac70ac2d4
triagung128 <triagung128@gmail.com>
2022-08-14 21:41:18 +0700

    rename folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/widgets/soal_1.dart';
-import 'package:learn_flutter/widgets/soal_10.dart';
-import 'package:learn_flutter/widgets/soal_11.dart';
-import 'package:learn_flutter/widgets/soal_12.dart';
-import 'package:learn_flutter/widgets/soal_13.dart';
-import 'package:learn_flutter/widgets/soal_14.dart';
-import 'package:learn_flutter/widgets/soal_15.dart';
-import 'package:learn_flutter/widgets/soal_16.dart';
-import 'package:learn_flutter/widgets/soal_17.dart';
-import 'package:learn_flutter/widgets/soal_18.dart';
-import 'package:learn_flutter/widgets/soal_19.dart';
-import 'package:learn_flutter/widgets/soal_2.dart';
-import 'package:learn_flutter/widgets/soal_20.dart';
-import 'package:learn_flutter/widgets/soal_21.dart';
-import 'package:learn_flutter/widgets/soal_23.dart';
-import 'package:learn_flutter/widgets/soal_22.dart';
-import 'package:learn_flutter/widgets/soal_24.dart';
-import 'package:learn_flutter/widgets/soal_3.dart';
-import 'package:learn_flutter/widgets/soal_4.dart';
-import 'package:learn_flutter/widgets/soal_5.dart';
-import 'package:learn_flutter/widgets/soal_6.dart';
-import 'package:learn_flutter/widgets/soal_7.dart';
-import 'package:learn_flutter/widgets/soal_8.dart';
-import 'package:learn_flutter/widgets/soal_9.dart';
+import 'package:learn_flutter/latihan_widget_dasar/soal_24.dart';
 
 void main() => runApp(const MyApp());
 
